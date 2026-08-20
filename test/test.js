7ba1ce89ca87097c754e19a942598f93c75e6001
Aditya123-kumar <akaditya0101@gmail.com>
2026-08-20 16:00:21 +0530

    update test for versioned response

--- test/test.js
+++ test/test.js
@@ -1,7 +1,16 @@
 var request = require('supertest');
 var app = require('../index.js');
+
 describe('GET /will', function() {
     it('respond with hello world', function(done) {
-        request(app).get('/will').expect('{ "response": "Hello World" }', done);
+        request(app)
+            .get('/will')
+            .expect(200)
+            .expect(function(res) {
+                if (res.body.response !== 'Hello World') {
+                    throw new Error('Unexpected response');
+                }
+            })
+            .end(done);
     });
 });
